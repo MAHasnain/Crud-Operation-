9c89e137394a74374cd6c40557cd895462cc47b4
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2023-08-05 23:41:09 +0500

    changing

--- server.mjs
+++ server.mjs
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
-import { customAlphabet } from "nanoid";
-const nanoid = customAlphabet("1234567890", 20);
+// import { customAlphabet } from "nanoid";
+// const nanoid = customAlphabet("1234567890", 20);
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import morgan from "morgan";
 dotenv.config();
 console.log(process.env.MONGODB_USERNAME);
 
-const mongodbURI = `mongodb+srv://attaulhasnain:Hasnain1881@cluster0.j7fos14.mongodb.net/?retryWrites=true&w=majority`;
+const mongodbURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.j7fos14.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(mongodbURI);
 const database = client.db("ecom");
 const productsCollection = database.collection("products");
@@ -15,60 +16,54 @@ const productsCollection = database.collection("products");
 const app = express();
 app.use(cors());
 app.use(express.json()); /// data ko json me convert krta h
-// let products=[];
+app.use (morgan("combined"));
 
-app.get("/checkAllProducts", async (req, res) => {
+app.get("/products", async (req, res) => {
  const cur = productsCollection.find({});
- const allvalues = await cur.toArray(); /// keyword "find" in mongo db docs
- res.send({
-  message: "all products",
-  data: allvalues,
- });
+
+ try {
+   const allvalues = await cur.toArray(); /// keyword "find" in mongo db docs
+   res.send({
+     message: "all products",
+     data: allvalues,
+    });
+   } catch (error) {
+    res.status(500).send({
+     message: "internal server error",
+    });
+   }
 });
 
+
+////  get request (any 1 product) ///
 app.get("/product/:id", async (req, res) => {
- const cur = await productsCollection.findOne({
-  _id: new ObjectId(req.params.id),
- });
- res.send({
-  message: "product found with id",
-  data: cur,
- });
+ if (!ObjectId.isValid(req.params.id)) {
+  res.status(403).send({ message: "incorrect product id" });
+  return;
+ }
+
+ try {
+  const productData = await productsCollection.findOne({
+   _id: new ObjectId(req.params.id),
+  });
+  res.send({
+   message: "product found",
+   data: productData,
+  });
+ } catch (error) {
+  console.log("error", error);
+  res.status(500).send({ message: "failed to get product, please try later" });
+ }
 });
-//console.log(typeof req.params.id)
 
-// if (isNaN(req.params.id)) {                      /// for validation
-//   res.status(403).send("invalid product id")
+// let students = () => {
+//   console.log("hello");
 // }
-
-// let isFound = false;
-// for (let i = 0; i < products.length; i++) {         //// i = index ( array me product ka number )
-//   if (products[i].id === req.params.id) {
-//     isFound = i;
-//     break;
-//   }
-// }
-
-
-
-
-//   if (isFound === false) {
-//     res.status(404);
-//     res.send({
-//       message: "product not found"
-//     })
-//   } else {
-//     res.send({
-//       message: "product found with id: " + products [isFound].id,
-//       data: products[isFound]
-//     });
-//   }
-// });
 
 ////  post request ////
 
-app.post("/", async (req, res) => {
- if (!req.body.name || !req.body.price || !req.body.description) {
+app.post("/product", async (req, res) => {
+ if (!req?.body?.name || !req?.body?.price || !req?.body?.description) {
   res.status(403)
    .send(`required parameter missing. example JSON request body : {
       name : "abc product",
@@ -78,40 +73,27 @@ app.post("/", async (req, res) => {
   return;
  }
 
- const { name } = req.body.name;
- const { price } = req.body.price;
- const { description } = req.body.description;
-
- // console.log(testing);
- // res.send ("validation pass")
- // return;
-
- // products.push({
- //   id : nanoid(),
- //   name : req.body.name,
- //   price : req.body.price,
- //   description : req.body.description
- // })
-
- await productsCollection
-  .insertOne({
-   name: req.body.name,
-   price: req.body.price,
-   description: req.body.description,
-  })
-  .then(() => {
-   res.status(201).send({ message: "created product" });
-  })
-  .catch(() => {
+ try {
+   const result = await productsCollection.insertOne({
+     name: req?.body?.name,
+     price: req?.body?.price,
+     description: req?.body?.description,
+    })
+    res.status(201).send({ message: "created product", data: result });
+  } catch (error) {
    res.status(500).send({
-    message: "internal server error",
+    message: "failed to add please try later"
    });
-  });
+  }
 });
 
 ////  put request ////
 
 app.put("/product/:id", async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)){
+   res.status(404).send({ message: "incorrect product id" });
+   return;
+  }
  if (!req.body.name && !req.body.price && !req.body.description) {
   res.status(403).send(`required parameter missing. 
       atleast one parameter is required: name, price or description to complete update 
@@ -122,82 +104,48 @@ app.put("/product/:id", async (req, res) => {
         description : "abc product description"
       }`);
   return;
- }
-
- //  const { name } = req.body.name;
- //  const { price } = req.body.price;
- //  const { description } = req.body.description;
-
- // let isFound = false;
-
- // for (let i = 0; i < products.length; i++) {
- //   if (products[i].id === req.params.id) {
- //     isFound = i;
- //     break;
- //   }
- // }
-
- // if (isFound === false) {
- //   res.status(404);
- //   res.send({
- //     message : "product not found",
-
- //   });
- // } else {
-
- //   if (req.body.name) products[isFound].name = req.body.name
- //   if (req.body.price) products[isFound].price = req.body.price
- //   if (req.body.description) products[isFound].description = req.body.description
-
- await productsCollection
-  .updateOne(
+ } 
+ 
+ let product = {}
+ 
+ if (req.body.name) req.body.name = req.body.name
+ if (req.body.price) req.body.price = req.body.price
+ if (req.body.description) req.body.description = req.body.description
+ 
+try{
+ const productdata = await productsCollection.updateOne(
    { _id: new ObjectId(req.params.id) },
-   {
-    $set: {
-     name: req.body.name,
-     price: req.body.price,
-     description: req.body.description,
-    },
-   }
+   {$set: product}
   )
-  .then(() => {
-   res.send({ message: "product is updated:" });
-  })
-  .catch(() => {
-   res.status(500).send({
-    message: "internal server error",
+  console.log("product updated ", productdata);
+   res.send({ message: "product updated successfully" });
+  }
+  catch {
+    console.log("error", error);
+   res.status(500).send({message: "internal server error",
    });
-  });
+  }
 });
 
 ////  delete request ////
 
 app.delete("/product/:id", async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)){
+    res.status(403).send({message : "incorrect product id"})
+    return;
+  }
+try {
+  const productdata = await productsCollection.deleteOne({ _id: new ObjectId(req.params.id) }); 
+  console.log("product deleted successfully", productdata);
 
- //  let isFound = false;
-
- //  for (let i = 0; i < products.length; i++) {
- //   if (products[i].id === req.params.id) {
- //    isFound = i;
- //    break;
- //   }
- //  }
- await productsCollection.deleteOne({ _id: new ObjectId(req.params.id) 
-  }).then(() => {
-   res.send({
-    message: "product is deleted",
-   });
-   res.status(404);
-  }).catch(() => {
-   res.send({
-    message: "product not found",
-   });
-  });
+    res.send({
+      message: "product deleted successfully"
+    });
+  }catch (error) {
+    console.log("error", error);
+    res.status(500).send({message: "failed to delete product, please try later"})
+  }
 });
-//  if (isFound === false) {
-//  } else {
-//   products.splice(isFound, 1);
-// }
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
